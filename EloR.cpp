eb4bfe2b653eb73f9c2305c22e840e9529fc6530
Aram Ebtekar <aramebtech@gmail.com>
2015-12-01 14:27:07 -0500

    updates and Goodbye 2013 ratings

--- EloR.cpp
+++ EloR.cpp
@@ -1,5 +1,4 @@
 // author: Aram Ebtekar
-// last updated: Nov 28, 2015
 #include <iostream>
 #include <fstream>
 #include <cassert>
@@ -12,12 +11,12 @@
 #include <algorithm>
 using namespace std;
 
-const double sig_newbie = 400; // starting uncertainty for new player
-const double sig_limit = 200; // limiting uncertainty for a player who competed a lot
-const double sig_perf = 500; // variation in individual performances
-//const double sig_noise = sig_limit*sig_limit / sqrt(sig_limit*sig_limit + sig_perf*sig_perf);
-const double sig_noise = sqrt(1.0 / (1.0/sig_limit/sig_limit - 1.0/sig_perf/sig_perf) - sig_limit*sig_limit);
+const double sig_limit = 100; // limiting uncertainty for a player who competed a lot
+const double sig_perf = 250; // variation in individual performances
+const double sig_noise = sqrt( 1.0 / (1.0/sig_limit/sig_limit - 1.0/sig_perf/sig_perf)
+                               - sig_limit*sig_limit );
 const double tfactor = sqrt(12.0)/M_PI;
+// ignore this: 400*PI / (ln(10)*sqrt(3)) = sqrt(1 / (1/sig_limit^2 - 1/sig_perf^2) + sig_perf^2)
 
 struct Rating
 {
@@ -25,14 +24,6 @@ struct Rating
     double sig; // uncertainty of skill belief
     double tig; // uncertainty converted into units suitable for the tanh function
     Rating(double m, double s) : mu(m), sig(s), tig(tfactor*s) {} // create player
-    Rating() : Rating(1500, sig_newbie) {} // create new player
-    int conservativeRating() { return int(mu + 2*(sig_limit - sig)); } // displayed rating
-    void add_noise() // prepare a prior for the current match
-    {
-        double varSkill = sig*sig + sig_noise*sig_noise;
-        sig = sqrt(varSkill);
-        tig = tfactor * sqrt(varSkill + sig_perf*sig_perf);
-    }
 };
 
 ostream& operator<<(ostream& os, const Rating& r)
@@ -41,13 +32,18 @@ ostream& operator<<(ostream& os, const Rating& r)
     return os;
 }
 
+double sum_sigInvSq(const vector<Rating>& ratings)
+{
+    double res = 0;
+    for (const Rating& r : ratings)
+        res += 1.0 / r.sig / r.sig;
+    return res;
+}
+
 // apply noise to one variable for which we have many estimates
 void add_noise_uniform(vector<Rating>& measurements)
 {
-    double sum_sigInvSq = 0;
-    for (Rating& r : measurements)
-        sum_sigInvSq += 1.0 / r.sig / r.sig;
-    double decay = sqrt(1.0 + sig_noise*sig_noise*sum_sigInvSq);
+    double decay = sqrt(1.0 + sig_noise*sig_noise*sum_sigInvSq(measurements));
     for (Rating& r : measurements)
     {
         r.sig *= decay;
@@ -56,14 +52,14 @@ void add_noise_uniform(vector<Rating>& measurements)
 }
 
 // returns something near the mean if the ratings are consistent; near the median if they're far apart
-double robustMean(vector<Rating> ratings, double offset = 0.0)
+double robustMean(const vector<Rating>& ratings, double offset = 0.0)
 {
-    double lo = 0, hi = 4000;
+    double lo = -1000, hi = 5000;
     while (hi - lo > 1e-9)
     {
         double mid = (lo + hi) / 2;
         double sum = 0;
-        for (Rating& r : ratings)
+        for (const Rating& r : ratings)
             sum += tanh((mid-r.mu)/r.tig) / r.tig;
         if (sum > offset)
             hi = mid;
@@ -92,16 +88,32 @@ struct Player
 {
     vector<Rating> perfs;
     Rating posterior;
+    int prevRating;
     void updatePosterior()
     {
-        double sum_sigInvSq = 0.0;
-        for (Rating& p : perfs)
-            sum_sigInvSq += 1.0 / p.sig / p.sig;
-        posterior = Rating(robustMean(perfs), 1.0/sqrt(sum_sigInvSq));
+        double mu = robustMean(perfs);
+        double sig = 1.0 / sqrt(sum_sigInvSq(perfs));
+        posterior = Rating(mu, sig);
     }
-    Player()
+    Player() : posterior(Rating(3,3)) // garbage initialization value
     {
-        perfs.push_back(posterior);
+        // initialize prior belief
+        // 1050 = sqrt(9)*(sig_limit + (1500-1000)/2)
+        perfs.reserve(9);
+        perfs.emplace_back(1000, 1050);
+        perfs.emplace_back(1200, 1050);
+        perfs.emplace_back(1350, 1050);
+        perfs.emplace_back(1450, 1050);
+        perfs.emplace_back(1500, 1050);
+        perfs.emplace_back(1550, 1050);
+        perfs.emplace_back(1650, 1050);
+        perfs.emplace_back(1800, 1050);
+        perfs.emplace_back(2000, 1050);
+        updatePosterior();
+    }
+    int conservativeRating() const // displayed rating
+    {
+        return int(posterior.mu - 2*(posterior.sig - sig_limit) + 0.5);
     }
 };
 
@@ -109,51 +121,70 @@ void simulateCodeforcesHistory()
 {
     map<string, Player> players;
     
-    for (int roundNum = 1; roundNum <= 602; ++roundNum)
+    for (int roundNum = 1; roundNum <= 604/*379*/; ++roundNum)
     {
         if (roundNum == 589 || roundNum == 598 || roundNum == 600)
             continue;
+        
+        // read the standings
         stringstream ssFileName;
         ssFileName << "Standings/" << roundNum << ".txt";
         ifstream standingsFile;
-        int N;
         standingsFile.open(ssFileName.str());
+        int N;
         standingsFile >> N;
-        cerr << "Processing Codeforces round " << roundNum;
+        cerr << "Processing Codeforces Round " << roundNum;
         cerr << " with " << N << " contestants..." << endl;
         
-        vector<Rating> ratings(N);
         vector<string> names(N);
         vector<int> lo(N), hi(N);
+        vector<Rating> compRatings;
+        compRatings.reserve(N);
         for (int i = 0; i < N; ++i)
         {
             standingsFile >> names[i] >> lo[i] >> hi[i];
             --lo[i]; --hi[i];
-            ratings[i] = players[names[i]].posterior;
+            
+            Player& player = players[names[i]];
+            Rating& r = player.posterior;
+            player.prevRating = player.conservativeRating();
+            double compVar = r.sig*r.sig + sig_noise*sig_noise + sig_perf*sig_perf;
+            compRatings.emplace_back(r.mu, sqrt(compVar));
         }
         standingsFile.close();
+        
+        // begin rating updates
         for (int i = 0; i < N; ++i)
         {
             Player& player = players[names[i]];
             add_noise_uniform(player.perfs);
             
-            double perf = performance(ratings, i, lo[i], hi[i]);
+            double perf = performance(compRatings, i, lo[i], hi[i]);
             player.perfs.emplace_back(perf, sig_perf);
             
             player.updatePosterior();
         }
+        // end rating updates
     }
-    vector<pair<double,string> > conservativeRatings;
-    for (pair<const string,Player>& player : players)
+    // output the result
+    double sumRatings = 0;
+    vector<tuple<int,int,int,string> > conservativeRatings;
+    for (pair<const string,Player>& entry : players)
     {
-        Rating& r = player.second.posterior;
-        conservativeRatings.push_back(make_pair(r.conservativeRating(), player.first));
+        Player& player = entry.second;
+        Rating& r = player.posterior;
+        conservativeRatings.push_back(make_tuple(
+            player.conservativeRating(), player.prevRating, player.perfs.back().mu, entry.first));
+        sumRatings += r.mu;
     }
+    cout << "Mean rating.mu = " << (sumRatings/players.size()) << endl;
     sort(conservativeRatings.begin(), conservativeRatings.end());
     reverse(conservativeRatings.begin(), conservativeRatings.end());
-    for (pair<double,string>& entry: conservativeRatings)
+    for (tuple<int,int,int,string>& entry: conservativeRatings)
     {
-        cout << entry.first << ' ' << entry.second << endl;
+        int delta = get<0>(entry) - get<1>(entry);
+        cout << get<0>(entry) << " " << get<3>(entry) << "\t\t\t\tp=" << get<2>(entry);
+        cout << "\tdelta=" << delta << endl;
     }
 }
 
