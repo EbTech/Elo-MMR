bb82c5a1fff611ec57c9620bae2bfd0a763c15fa
inutard <paul.liu.ubc@gmail.com>
2020-08-26 23:33:04 -0700

    Basic reddit dataset working.

--- ranking/src/main.rs
+++ ranking/src/main.rs
@@ -10,7 +10,7 @@ use std::collections::HashMap;
 /// somewhat longer if accessing the Codeforces API
 fn main() {
     let mut players = HashMap::new();
-    let config = get_contest_config(ContestSource::Codeforces);
+    let config = get_contest_config(ContestSource::Reddit);
     for contest_id in get_contest_ids(&config.contest_id_file) {
         let contest = get_contest(&config.contest_cache_folder, contest_id);
         println!(
