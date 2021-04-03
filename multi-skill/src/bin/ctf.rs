f6e5e5f2f819e9500edcd48bbc5dc23092e7b8ef
Aram Ebtekar <aramebtech@gmail.com>
2021-04-02 23:21:20 -0700

    generate ctf contest ids file

--- multi-skill/src/bin/ctf.rs
+++ multi-skill/src/bin/ctf.rs
@@ -1,3 +1,4 @@
+use multi_skill::data_processing::write_slice_to_file;
 use reqwest::blocking::{Client, RequestBuilder};
 use reqwest::StatusCode;
 use select::document::Document;
@@ -21,27 +22,24 @@ struct ArchiveRows<'a, I: Iterator<Item = Node<'a>>> {
 }
 
 impl<'a, I: Iterator<Item = Node<'a>>> Iterator for ArchiveRows<'a, I> {
-    type Item = (String, String, String);
+    type Item = usize;
 
     fn next(&mut self) -> Option<Self::Item> {
         let url_cell = self.table.next()?;
-        let date_cell = self.table.next().unwrap();
-        let missing_cell = self.table.nth(4).unwrap();
+        let missing_cell = self.table.nth(5).unwrap();
         if missing_cell.text().starts_with("missing") {
             // This contest has no scoreboard, so skip it
             self.next()
         } else {
             let node = url_cell.select(Name("a")).next().unwrap();
             let path = node.attr("href").unwrap();
-            let url = format!("https://ctftime.org{}", path);
-            let title = node.text();
-            let time_string = date_cell.text().split(" — ").nth(1).unwrap().to_owned();
-            Some((url, title, time_string))
+            let id = path.strip_prefix("/event/").unwrap();
+            Some(id.parse().unwrap())
         }
     }
 }
 
-struct ScoreboardRows<'a, I: Iterator<Item = Node<'a>>> {
+/*struct ScoreboardRows<'a, I: Iterator<Item = Node<'a>>> {
     table: I,
 }
 
@@ -56,30 +54,37 @@ impl<'a, I: Iterator<Item = Node<'a>>> Iterator for ScoreboardRows<'a, I> {
     }
 }
 
-fn process_heat(client: &Client, heat_page: Document) {
-    tracing::info!("EXTRACTED {:?}", heat_page);
-}
+for (contest_url, title, time_string) in table {
+    tracing::info!("{} {} {}", contest_url, title, time_string);
+    let contest_req = client.get(contest_url);
+    let contest_page = request(contest_req).expect("Failed HTTP status");
+    let title2 = contest_page.select(Name("h2")).next().unwrap().text();
+    assert_eq!(title, title2);
+    let time2 = contest_page.select(Name("p")).next().unwrap().text();
+    let time2 = time2.split("— ").nth(1).unwrap();
+    let time2 = time2.split(" UTC").next().unwrap();
+    assert_eq!(time_string, time2);
+    let table = ScoreboardRows {
+        table: year_page.select(Name("td")),
+    };
+}*/
 
 fn main() {
     tracing_subscriber::fmt::init();
 
     let client = Client::new();
-
-    for year in 2005..=2021 {
-        let year_url = format!("https://ctftime.org/event/list/?year={}", year);
-        let year_req = client.get(year_url);
-        let year_page = request(year_req).expect("Failed HTTP status");
-        let table = ArchiveRows {
-            table: year_page.select(Name("td")),
-        };
-        // TODO: reverse rows, parse time in seconds, parse Contest
-        for (contest_url, title, time_string) in table {
-            tracing::info!("{} {} {}", contest_url, title, time_string);
-            let contest_req = client.get(contest_url);
-            let contest_page = request(contest_req).expect("Failed HTTP status");
-            let table = ScoreboardRows {
-                table: year_page.select(Name("td")),
+    let contest_ids = (2005..=2021)
+        .flat_map(|year| {
+            let url = format!("https://ctftime.org/event/list/?year={}", year);
+            let req = client.get(url);
+            let page = request(req).expect("Failed HTTP status");
+            let table = ArchiveRows {
+                table: page.select(Name("td")),
             };
-        }
-    }
+            let urls = table.collect::<Vec<_>>();
+            urls.into_iter().rev()
+        })
+        .collect::<Vec<_>>();
+
+    write_slice_to_file(&contest_ids, "../data/ctf_contest_ids.json");
 }
---
+++ multi-skill/src/data_processing/ctf_parse.rs
@@ -0,0 +1,116 @@
+use super::Contest;
+use reqwest::blocking::Client;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::convert::{TryFrom, TryInto};
+
+fn ctftime_human_url(contest_id: usize) -> String {
+    format!("https://ctftime.org/event/{}", contest_id)
+}
+
+impl TryFrom<Vec<CFRatingChange>> for Contest {
+    type Error = String;
+
+    /// Checks the integrity of our API response and convert it into a more convenient format.
+    fn try_from(json_contest: Vec<CFRatingChange>) -> Result<Self, Self::Error> {
+        let first_change = json_contest.get(0).ok_or("Empty standings")?;
+        let id = first_change.contest_id;
+        let name = first_change.contest_name.clone();
+        let time_seconds = first_change.rating_update_time_seconds;
+
+        let mut lo_rank = json_contest.len() + 1;
+        let mut hi_rank = json_contest.len() + 1;
+        let mut seen_handles = HashMap::with_capacity(json_contest.len());
+        let mut standings = Vec::with_capacity(json_contest.len());
+
+        for (i, mut change) in json_contest.into_iter().enumerate().rev() {
+            if id != change.contest_id {
+                return Err(format!(
+                    "Inconsistent contests ids {} and {}",
+                    id, change.contest_id
+                ));
+            }
+            if name != change.contest_name {
+                return Err(format!(
+                    "Inconsistent contest names {} and {}",
+                    name, change.contest_name
+                ));
+            }
+            if time_seconds != change.rating_update_time_seconds {
+                // I don't know why but contests 61,318,347,373,381,400,404,405
+                // each contain one discrepancy, usually 4 hours late
+                tracing::warn!(
+                    "@ {}: Inconsistent contest times {} and {}",
+                    id,
+                    time_seconds,
+                    change.rating_update_time_seconds
+                );
+            }
+            while let Some(j) = seen_handles.insert(change.handle.clone(), i) {
+                // I don't know why but contests 447,472,615 have duplicate users
+                if !(id == 447 || id == 472 || id == 615) {
+                    return Err(format!(
+                        "Duplicate user {} at positions {} and {}",
+                        change.handle, i, j
+                    ));
+                }
+                tracing::warn!(
+                    "@ {}: duplicate user {} at positions {} and {}",
+                    id,
+                    change.handle,
+                    i,
+                    j
+                );
+                change.handle += "_clone";
+            }
+
+            if lo_rank == change.rank {
+                if !(lo_rank < i + 2 && i < hi_rank) {
+                    return Err(format!(
+                        "Position {} is not between ranks {} and {}",
+                        i + 1,
+                        lo_rank,
+                        hi_rank
+                    ));
+                }
+            } else {
+                if !(change.rank < lo_rank && lo_rank == i + 2) {
+                    return Err(format!("Invalid start of rank {}", lo_rank));
+                }
+                hi_rank = lo_rank;
+                lo_rank = change.rank;
+            }
+
+            standings.push((change.handle, lo_rank - 1, hi_rank - 2));
+        }
+        standings.reverse();
+
+        Ok(Self {
+            name,
+            url: Some(codeforces_human_url(id)),
+            weight: 1.0,
+            time_seconds,
+            standings,
+        })
+    }
+}
+
+/// Retrieves the Codeforces contest with the given ID, or panics if the API call fails.
+/// Codeforces documentation: https://codeforces.com/apiHelp/methods#contest.ratingChanges
+pub fn fetch_cf_contest(client: &Client, contest_id: usize) -> Contest {
+    let response = client
+        .get(&ctftime_human_url(contest_id))
+        .send()
+        .expect("Connection error: is Codeforces.com down?")
+        .error_for_status()
+        .expect("Status error: is Codeforces.com down?");
+    let packet: CFResponse<Vec<CFRatingChange>> = response
+        .json()
+        .expect("Codeforces API response doesn't match the expected JSON schema");
+    match packet {
+        CFResponse::Ok { result } => result
+            .try_into()
+            .expect("Failed to parse JSON response as a valid Contest"),
+        CFResponse::Failed { comment } => panic!("{}", comment),
+    }
+}
